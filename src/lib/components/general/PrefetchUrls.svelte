56c287dce48ff971a2106317938fab147d87229c
Sam Welsh <sammwelsh@ymail.com>
2024-01-27 04:04:10 -0800

    prefetch

---
+++ src/lib/components/general/PrefetchUrls.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let prefetchUrls: string[];
+</script>
+
+<div class="hidden">
+  {#each prefetchUrls as href}
+    <link rel="prefetch" {href} />
+  {/each}
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Icon } from './general/Icon.svelte';
+export { default as PrefetchUrls } from './general/PrefetchUrls.svelte';
