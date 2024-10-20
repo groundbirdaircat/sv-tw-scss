dac5cc7e21fca35ffbe28de6088ab095fee836a0
Sam Welsh <sammwelsh@ymail.com>
2024-10-20 01:07:57 -0700

    update eslint

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true
+        }
+      ]
+    }
+  },
+  {
+    ignores: ['build/', '.svelte-kit/', 'dist/']
+  }
+);
