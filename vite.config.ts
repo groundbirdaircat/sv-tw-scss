0aee5fc380f5c71f164a26b706e52c101d8d8d24
Sam Welsh <sammwelsh@ymail.com>
2024-10-20 00:10:04 -0700

    update deps - svelte 5 official release

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@use "src/styles" as *;'
+        additionalData: '@use "@/styles" as *;',
+        api: 'modern-compiler'
       }
     }
   }
