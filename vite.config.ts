25780ec76232323c8f2051908cb774f3752a41a3
Sam Welsh <sammwelsh@ymail.com>
2025-05-09 22:15:00 -0700

    update deps, add devtoolsJson vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss()],
+  plugins: [sveltekit(), tailwindcss(), devtoolsJson()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
