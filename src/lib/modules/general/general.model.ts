c4af1f5b643d9d450df8b061b9bdeed77bd14e22
Sam Welsh <sammwelsh@ymail.com>
2026-05-18 04:21:37 -0700

    refine type helpers

--- src/lib/modules/general/general.model.ts
+++ src/lib/modules/general/general.model.ts
@@ -1,13 +1,16 @@
 import type { Color } from '$modules/general/color';
 import { Icon } from '$modules/general/icon';
 
+/**
+ * **CurrentTarget<event?, element?>**
+ */
+export type CurrentTarget<R extends Event = Event, T extends HTMLElement = HTMLElement> = R & {
+  currentTarget: T;
+};
+
 export type SlotContent = () => any; //eslint-disable-line
 
 export type AnyFn = (...args: any[]) => any; //eslint-disable-line
-
-export type EventWithTarget<T> = Event & { currentTarget: T };
-
-export type WithTarget<R extends Event, T> = R & { currentTarget: T };
 
 export type TimeoutId = ReturnType<typeof setTimeout>;
 
@@ -16,3 +19,5 @@ export type Dict<T = string> = Record<string, T>;
 export type ColorValue = (typeof Color)[keyof typeof Color];
 
 export type Icons = keyof typeof Icon;
+
+export type ConstEnumValues<T> = T[keyof T];
